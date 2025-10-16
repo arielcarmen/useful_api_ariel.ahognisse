89fb012df214f921ad907248ff0333b20225d623
Ariel Carmen <arielahognisse@gmail.com>
2025-10-16 12:43:52 +0100

    feat: configure module active state middleware

--- app/Http/Middleware/CheckModuleActive.php
+++ app/Http/Middleware/CheckModuleActive.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\UserModule;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckModuleActive
@@ -13,8 +15,13 @@ class CheckModuleActive
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, string $module_id): Response
     {
+        $user_module = UserModule::where([['user_id', Auth::id()], ['module_id', $module_id]])->first();
+
+        if (!$user_module || $user_module->active === 0) {
+            return response(['error' => 'Module inactive. Please activate this module to use it.'], 403);
+        }
         return $next($request);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ModuleController;
+use App\Http\Middleware\CheckModuleActive;
 use App\Models\Module;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
