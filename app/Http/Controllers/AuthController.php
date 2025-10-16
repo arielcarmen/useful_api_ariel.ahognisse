e1003e3c7e297cabfb9e8090d5a2d5e8b3139bff
Ariel Carmen <arielahognisse@gmail.com>
2025-10-16 11:02:21 +0100

    feat: create login feature with its formrequest and route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -25,5 +26,20 @@ class AuthController extends Controller
             'email' => $user->email,
             'created_at' => $user->created_at
         ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $request->validated();
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || ! Hash::check($request->password, $user->password)) {
+            return response('These credentials don\'t match with our records', 401);
+        }
+        
+        $token = $user->createToken($user->name);
+
+        return response(['token' => $token->plainTextToken, 'user_id' => $user->id]);
     }
 }
